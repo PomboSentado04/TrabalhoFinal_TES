12c34500cc6e5576a66b1085a902ca8a243aae23
Pedro Nunes Moreira <pedronunesmm04@gmail.com>
2025-06-10 18:55:04 -0300

    Adicionado OnModelCreating, com as especificações de tabela e relações. Mudado o tipo da propriedade Nota de int para double.

--- Backend/API/Models/AppDataContext.cs
+++ Backend/API/Models/AppDataContext.cs
@@ -16,5 +16,40 @@ namespace API.Models
         {
             optionsBuilder.UseSqlite("Data Source=AvaliacaoFilmesSeries.db");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuração das relações
+            modelBuilder.Entity<Avaliacao>()
+                .HasOne(a => a.FilmeSerie)
+                .WithMany(f => f.Avaliacoes)
+                .HasForeignKey(a => a.FilmeSerieId);
+
+            modelBuilder.Entity<Avaliacao>()
+                .HasOne(a => a.Usuario)
+                .WithMany(u => u.Avaliacoes)
+                .HasForeignKey(a => a.UsuarioId);
+
+            // Configurações adicionais para tamanho máximo de campos
+            modelBuilder.Entity<FilmeSerie>()
+                .Property(f => f.Titulo)
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<FilmeSerie>()
+                .Property(f => f.Diretor)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Usuario>()
+                .Property(u => u.Nome)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Usuario>()
+                .Property(u => u.Email)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Avaliacao>()
+                .Property(a => a.Comentario)
+                .HasMaxLength(1000);
+        }
     }
 }
