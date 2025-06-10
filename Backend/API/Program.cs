05d36943b5c780794c8cda6b5c14215f31e5f726
Pedro Nunes Moreira <pedronunesmm04@gmail.com>
2025-06-10 19:41:01 -0300

    Adicionado a configuração para usar o banco de dados. Adicionado a configuração de poilítica permissiva do CORS.

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,6 +1,22 @@
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do SQLite e CORS
+builder.Services.AddDbContext<AppDataContext>(options => 
+    options.UseSqlite("Data Source=AvaliacaoFilmesSeries.db"));
+
+builder.Services.AddCors(options =>
+    options.AddPolicy("Acesso Total",
+        configs => configs
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod())
+);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("Acesso Total");
 
 app.Run();
